ce2dbf2fb7f68fc6bae49301deafc5cf8b0f35cb
Vinicius Tozzi <viniciustozzi@pm.me>
2022-04-25 18:32:59 +0200

    Collision between enemies and shots working
    Now both shots and enemies disappear from the screen when they collide
    with each other.

--- src/my_quill_sketch/enemies.clj
+++ src/my_quill_sketch/enemies.clj
@@ -1,10 +1,10 @@
 (ns my-quill-sketch.enemies
   (:require [my-quill-sketch.utils :as utils]
-            [my-quill-sketch.player :as p]))
+            [my-quill-sketch.player :as p]
+            [clojure.set :refer [difference]]))
 
 (defn make-enemy [x y]
-  {:id (utils/gen-id)
-   :x x
+  {:x x
    :y y
    :size 32
    :hitbox 32})
@@ -20,16 +20,17 @@
                          (utils/make-circle (:x enemy) (:y enemy) (:hitbox enemy)))
     (vector enemy shot)))
 
-(defn enemies-shots-collision [{:keys [shots enemies] :as state}]
-  (flatten (for [s shots
-                 e enemies]
-             (enemy-shot-collision s e))))
+(defn enemies-shots-collision [enemies shots]
+  (remove nil? (distinct (flatten (for [s shots
+                                        e enemies]
+                                    (enemy-shot-collision s e))))))
 
-(enemies-shots-collision state)
+
+;; (def state {:enemies [(make-enemy 10 10) (make-enemy 150 150)]
+;;             :shots [(p/make-shot 10 10) (p/make-shot 300 300)]})
 
 (defn check-collision-enemies->shot [{:keys [enemies shots] :as state}]
-  ;(assoc state :enemies (vec (remove #(collides-with-shot? shots %) enemies)))
-      ;(assoc :shots (remove (fn [s] (collides-with-enemy? enemies s) shots)))
-  )
-
-(vec (distinct (flatten [[1 2] [2 3]])))
+  (let [collisions (enemies-shots-collision enemies shots)]
+    (-> state
+        (assoc :enemies (vec (difference (set enemies) (set collisions))))
+        (assoc :shots (vec (difference (set shots) (set collisions)))))))
--- test/my_quill_sketch/test.clj
+++ test/my_quill_sketch/test.clj
@@ -99,7 +99,8 @@
 ;;
 (deftest test-enemy-shot-collision
   (testing "When enemies and shot collides"
-    (is (= {:enemies [(e/make-enemy 20 20)]
-            :shots [(p/make-shot 40 40)]}
-           (e/check-collision-enemies->shot {:enemies [(e/make-enemy 10 10) (e/make-enemy 150 150)]
-                                             :shots [(p/make-shot 10 10) (p/make-shot 300 300)]})))))
+    (is (= {:enemies [(e/make-enemy 150 150)]
+            :shots [(p/make-shot 300 300)]}
+           (e/check-collision-enemies->shot
+            {:enemies [(e/make-enemy 10 10) (e/make-enemy 150 150)]
+             :shots [(p/make-shot 10 10) (p/make-shot 300 300)]})))))
